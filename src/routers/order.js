d93e4d7852bd5b1c9059b161721579df1784582c
Mark <50715288+whymeoff@users.noreply.github.com>
2020-02-05 15:51:31 +0200

    Order post method

--- src/routers/order.js
+++ src/routers/order.js
@@ -13,9 +13,23 @@ router.get('/:id', auth.auth, authButtons, async (req, res) => {
         if (!cart) return res.redirect('/')
         const items = await (await cart.populate('items.item').execPopulate()).toObject().items
         calcSum(items)
-        res.render('order', { items, ...req.authButtons })
+        res.render('order', { items, ...req.authButtons, cartID: cart._id })
     } catch (e) {
+        return res.redirect('/')
+    }
+})
 
+router.post('/:id', auth.auth, async (req, res) => {
+    try {
+        const cart = await Cart.findById(req.params.id)
+        if (!cart) return res.redirect('/')
+
+        const order = new Order({ ...req.body, owner: cart.owner, items: cart.items })
+        await order.save()
+        await cart.remove()
+        res.redirect('/')
+    } catch (e) {
+        res.redirect(`/order/${cart._id}`)
     }
 })
 
