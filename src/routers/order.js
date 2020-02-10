2eb675f7f39d4428f427df81ac64db7c3ee0a069
Mark <50715288+whymeoff@users.noreply.github.com>
2020-02-10 21:11:43 +0200

    User orders route

--- src/routers/order.js
+++ src/routers/order.js
@@ -8,7 +8,24 @@ const Order = require('../models/order')
 
 const router = express.Router()
 
-router.get('/:id', auth.auth, authButtons, async (req, res) => {
+router.get('/orders', auth.auth, authButtons, async (req, res) => {
+    try {
+        let orders = await Order.find({ owner: req.user._id }).sort([['status', 1]])
+        orders.items = []
+
+        for (let i = 0; i < orders.length; i++) {
+            orders.items.push(await (await orders[i].populate('items.item').execPopulate()).toObject().items)
+            calcSum(orders.items[i])
+            orders[i].fullSum = orders.items[i].fullSum
+        }
+
+        res.render('orders', { orders, ...req.authButtons })
+    } catch (e) {
+
+    }
+})
+
+router.get('/order/:id', auth.auth, authButtons, async (req, res) => {
     try {
         const cart = await Cart.findById(req.params.id)
         if (!cart) return res.redirect('/')
@@ -20,7 +37,7 @@ router.get('/:id', auth.auth, authButtons, async (req, res) => {
     }
 })
 
-router.post('/:id', auth.auth, async (req, res) => {
+router.post('/order/:id', auth.auth, async (req, res) => {
     try {
         const cart = await Cart.findById(req.params.id)
         if (!cart) return res.redirect('/')
@@ -34,7 +51,7 @@ router.post('/:id', auth.auth, async (req, res) => {
     }
 })
 
-router.patch('/:id', isAdmin, async (req, res) => {
+router.patch('/order/:id', isAdmin, async (req, res) => {
     console.log(req.query)
     try {
         await Order.findByIdAndUpdate(req.params.id, { status: parseInt(req.query.status) })
